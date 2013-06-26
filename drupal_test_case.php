3e668d2545e4294229bcb0be27de07aeecfd801f
Sang <le@thanhsang.me>
2013-06-26 17:11:31 +0700

    Update for Drupal 7

--- drupal_test_case.php
+++ drupal_test_case.php
@@ -2302,7 +2302,7 @@ abstract class DrupalTestCase extends PHPUnit_Framework_TestCase {
    */
   protected function refreshVariables() {
     global $conf;
-    cache('bootstrap')->delete('variables');
+    cache_clear_all('variables', 'cache_bootstrap');
     $conf = variable_initialize();
   }
 
@@ -2385,7 +2385,7 @@ class DrupalUnitTestCase extends DrupalTestCase {
     if (!defined('DRUPAL_ROOT')) {
       define('DRUPAL_ROOT', UPAL_ROOT);
     }
-    require_once DRUPAL_ROOT . '/core/includes/bootstrap.inc';
+    require_once DRUPAL_ROOT . './includes/bootstrap.inc';
     drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
   }
 }
@@ -2489,7 +2489,7 @@ class DrupalWebTestCase extends DrupalTestCase {
     //$_SERVER['SERVER_SOFTWARE'] = NULL;
     //$_SERVER['HTTP_USER_AGENT'] = NULL;
 
-    require_once DRUPAL_ROOT . '/core/includes/bootstrap.inc';
+    require_once DRUPAL_ROOT . './includes/bootstrap.inc';
     drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
     // Enable modules for this test.
